4642cc8475a48342eb6e14871810a28f0b403bd0
CK <chamkank@users.noreply.github.com>
2018-02-17 14:00:39 -0800

    initialize express app

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+  res.json([{id: 1, username: "userA"}])
+});
+
+module.exports = router;
