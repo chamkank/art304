db8036ca07dc94dba9c3f05732556afb7236e4cc
Andrew Pang <v7n7@ugrad.cs.ubc.ca>
2018-03-23 18:38:35 -0700

    Return comment info

--- classes/Comment.js
+++ classes/Comment.js
@@ -5,6 +5,7 @@ comment = {};
 comment.getInfo = function (comment_id){
 	database.query("SELECT * FROM comment WHERE comment_id = ${comment_id}", (err, res) => {
 		console.log(res.rows[0]);
+		return res.rows[0];
 	})
 }
 
