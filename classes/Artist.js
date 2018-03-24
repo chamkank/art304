0299192d8919284f1f2c64f8879af9705c8f334a
Andrew Pang <v7n7@ugrad.cs.ubc.ca>
2018-03-23 17:56:49 -0700

    Comments, generting id, inserting into On and Comment

--- classes/Artist.js
+++ classes/Artist.js
@@ -2,13 +2,27 @@ var db = require('../database');
 
 artist = {};
 
-artist.postComment = function (artId, commentString){
-	database.query("INSERT INTO On", (err, res) => {
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 20; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+artist.postComment = function (artId, artist_username, commentString){
+	var commentId = makeid();
+	var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+	
+	database.query("INSERT INTO Comment(comment_id,artist_username,date_posted,comment_text) VALUES (${commentId},${artist_username}, ${date}, ${commentString})", (err, res) => {
 		console.log(res.rows[0]);
 	})
-	database.query("INSERT INTO Comment VALUES ()", (err, res) => {
+	database.query("INSERT INTO On(comment_id,artist_username,art_id) VALUES (${commentId}, ${artist_username},${artId}) ", (err, res) => {
 		console.log(res.rows[0]);
 	})
+
 }
 
 module.exports = artist;
