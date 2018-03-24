c48a6051dc0fbf465d8e0c682d609c1923f63132
CK <chamkank@users.noreply.github.com>
2018-03-24 16:15:32 -0700

    example of calling a function in /classes/

--- classes/tests/artist_test.js
+++ classes/tests/artist_test.js
@@ -1,15 +1,16 @@
 var artist = require('../artist');
 
-username = 'test3'
+username = 'test4'
 password = 'testpass'
 email_address = 'email@email.com'
 birth_date = '2018-05-17'
 
 var createUser = artist.createArtist(username, password, email_address, birth_date)
-createUser.then(function(res){
-    if (res){
+
+createUser.then(function (res) {
+    if (res) {
         console.log('Successfully added user!');
     }
-}, function(err){
+}).catch(function (err) {
     console.log(err);
 });
