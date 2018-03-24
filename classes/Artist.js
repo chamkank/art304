351f83311c416596dc7b333263916367f8cedb9e
CK <chamkank@users.noreply.github.com>
2018-03-24 15:57:36 -0700

    fix string matching, return promise

--- classes/Artist.js
+++ classes/Artist.js
@@ -1,4 +1,4 @@
-var db = require('../database');
+var database = require('../database');
 
 artist = {};
 
@@ -17,33 +17,38 @@ artist.postComment = function (artId, artist_username, commentString){
 	var commentId = makeid();
 	var datetime = new Date().toLocaleString();
 
-	database.query("INSERT INTO Comment(comment_id,artist_username,date_posted,comment_text) VALUES (${commentId},${artist_username}, ${datetime}, ${commentString})", (err, res) => {
+	database.query(`INSERT INTO Comment(comment_id,artist_username,date_posted,comment_text) VALUES ('${commentId}', '${artist_username}', '${datetime}', '${commentString}')`, (err, res) => {
 		console.log(res.rows[0]);
 	})
-	database.query("INSERT INTO Comments_On(comment_id,artist_username,art_id) VALUES (${commentId}, ${artist_username},${artId}) ", (err, res) => {
+	database.query(`INSERT INTO Comments_On(comment_id,artist_username,art_id) VALUES ('${commentId}', '${artist_username}','${artId}')`, (err, res) => {
 		console.log(res.rows[0]);
 	})
 
 }
 
 artist.deleteComment = function (commentId){
-	database.query("DELETE FROM Comment WHERE comment_id = ${commentId}", (err, res) => {
+	database.query(`DELETE FROM Comment WHERE comment_id = '${commentId}'`, (err, res) => {
 		console.log(res.rows[0]);
 	})
 }
 
 artist.getArt = function (username){
-	database.query("SELECT * FROM Art WHERE owner_username = ${username}}", (err, res) => {
+	database.query(`SELECT * FROM Art WHERE owner_username = '${username}'`, (err, res) => {
 		console.log(res.rows[0]);
 	})
 }
 
 artist.createArtist = function(username, password, email_address, birth_date){
-	var datetime = new Date().toLocaleString();
-	database.query("INSERT INTO Artist_Wall(username, password, email_address, date_joined, birth_date) VALUES (${artist_username}, ${password}, ${email_address}, ${datetime}, ${birth_date})", (err, res) => {
-		if (err) { return false; }
-		return true;
-	})
+	return new Promise(function(resolve, reject) {
+		var datetime = new Date().toLocaleString();
+		database.query(`INSERT INTO Artist_Wall(username, password, email_address, date_joined, birth_date) VALUES ('${username}', '${password}', '${email_address}', '${datetime}', '${birth_date}')`, (err, res) => {
+			if (err) { 
+				reject(err.detail); 
+			} else {
+				resolve(true);
+			}
+		})
+	});
 }
 
 module.exports = artist;
