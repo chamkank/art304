5fadd2a42e201f200ec30cd1fd67edbbfe8b5ad8
CK <chamkank@users.noreply.github.com>
2018-03-24 16:08:08 -0700

    make everything return promises in Artist.js

--- classes/Artist.js
+++ classes/Artist.js
@@ -4,46 +4,54 @@ artist = {};
 
 //Generates a random 20 digit id
 function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 20; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+	for (var i = 0; i < 20; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  return text;
+	return text;
 }
 
-artist.postComment = function (artId, artist_username, commentString){
+artist.postComment = function (artId, artist_username, commentString) {
 	var commentId = makeid();
 	var datetime = new Date().toLocaleString();
 
-	database.query(`INSERT INTO Comment(comment_id,artist_username,date_posted,comment_text) VALUES ('${commentId}', '${artist_username}', '${datetime}', '${commentString}')`, (err, res) => {
-		console.log(res.rows[0]);
-	})
-	database.query(`INSERT INTO Comments_On(comment_id,artist_username,art_id) VALUES ('${commentId}', '${artist_username}','${artId}')`, (err, res) => {
-		console.log(res.rows[0]);
-	})
-
+	return new Promise(function (resolve, reject) {
+		database.query(`INSERT INTO Comment(comment_id,artist_username,date_posted,comment_text) VALUES ('${commentId}', '${artist_username}', '${datetime}', '${commentString}')`, (err, res) => {
+			if (err) { reject(err.detail) }
+			database.query(`INSERT INTO Comments_On(comment_id,artist_username,art_id) VALUES ('${commentId}', '${artist_username}','${artId}')`, (err, res) => {
+				if (err) { reject(err.detail) }
+				else { resolve(true) }
+			});
+		});
+	});
 }
 
-artist.deleteComment = function (commentId){
-	database.query(`DELETE FROM Comment WHERE comment_id = '${commentId}'`, (err, res) => {
-		console.log(res.rows[0]);
+artist.deleteComment = function (commentId) {
+	return new Promise(function (resolve, reject) {
+		database.query(`DELETE FROM Comment WHERE comment_id = '${commentId}'`, (err, res) => {
+			if (err){ reject(err.detail) }
+			else { resolve(true) }
+		});
+	});
+}
+
+artist.getArt = function (username) {
+	return new Promise(function(resolve, reject){
+		database.query(`SELECT * FROM Art WHERE owner_username = '${username}'`, (err, res) => {
+			if (err){ reject(err.detail) }
+			else { resolve(res.rows) }
+		});
 	})
 }
 
-artist.getArt = function (username){
-	database.query(`SELECT * FROM Art WHERE owner_username = '${username}'`, (err, res) => {
-		console.log(res.rows[0]);
-	})
-}
-
-artist.createArtist = function(username, password, email_address, birth_date){
-	return new Promise(function(resolve, reject) {
+artist.createArtist = function (username, password, email_address, birth_date) {
+	return new Promise(function (resolve, reject) {
 		var datetime = new Date().toLocaleString();
 		database.query(`INSERT INTO Artist_Wall(username, password, email_address, date_joined, birth_date) VALUES ('${username}', '${password}', '${email_address}', '${datetime}', '${birth_date}')`, (err, res) => {
-			if (err) { 
-				reject(err.detail); 
+			if (err) {
+				reject(err.detail);
 			} else {
 				resolve(true);
 			}
