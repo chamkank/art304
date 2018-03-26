e9560066ef5fa83ce36844eb4495dab79454ca3e
Andrew Pang <pang.andrew.1991@gmail.com>
2018-03-25 17:27:01 -0700

    art.getComments query fix

--- classes/Art.js
+++ classes/Art.js
@@ -4,10 +4,9 @@ var tag = require('./Tag');
 art = {};
 
 art.getComments = function (art_id){
-	database.query("SELECT * FROM Comment, Comments_On WHERE Comments_On.comment_id = Comment.comment_id AND Comments_On.art_id = ${art_id} ORDER BY Date DESC, Time DESC", (err, res) => {
-		console.log(res.rows);
+	db.query("SELECT * FROM Comment, Comments_On WHERE Comments_On.comment_id = Comment.comment_id AND Comments_On.art_id = '"+art_id+"' ORDER BY date_posted", (err, res) => {
 		return res.rows;
-	})
+	});
 };
 
 
