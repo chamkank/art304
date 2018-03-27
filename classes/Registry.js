ef015bb446bd6f677f7fd2081ef9a4c155416507
CK <chamkank@users.noreply.github.com>
2018-03-26 17:51:18 -0700

    wrap Registry functions with promises

--- classes/Registry.js
+++ classes/Registry.js
@@ -4,42 +4,50 @@ registry = {};
 
 // returns all tags in database
 registry.getTags = () => {
-    db.query("SELECT * from Tag", (err, res) => {
-        if (err){
-            console.error("Error selecting all tags", err);
-        }
-        return res.rows;
-    });
+    return new Promise(function (resolve, reject) {
+        db.query("SELECT * from Tag", (err, res) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(res.rows);
+        })
+    })
 };
 
 // returns all art in database
 registry.getArt = () => {
-    db.query("SELECT * from Art", (err, res) => {
-        if (err){
-            console.error("Error selecting all art", err);
-        }
-        return res.rows;
-    });
+    return new Promise(function (resolve, reject) {
+        db.query("SELECT * from Art", (err, res) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(res.rows);
+        })
+    })
 };
 
 // returns all artists in database
 registry.getArtists = () => {
-    db.query("SELECT * from Artist_Wall", (err, res) => {
-        if (err){
-            console.error("Error selecting all artists", err);
-        }
-        return res.rows;
-    });
+    return new Promise(function (resolve, reject) {
+        db.query("SELECT * from Artist_Wall", (err, res) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(res.rows);
+        })
+    })
 };
 
 // returns all comments in database
-registry.getComment = () => {
-    db.query("SELECT * from Comment", (err, res) => {
-        if (err){
-            console.error("Error selecting all comments", err);
-        }
-        return res.rows;
-    });
+registry.getComments = () => {
+    return new Promise(function (resolve, reject) {
+        db.query("SELECT * from Comment", (err, res) => {
+            if (err) {
+                reject(err)
+            }
+        resolve(res.rows);
+        })
+    })
 };
 
 module.exports = registry;
