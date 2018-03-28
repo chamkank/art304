4a125a627028807d2df7d3b09a0bea1012c821c0
CK <chamkank@users.noreply.github.com>
2018-03-28 14:12:23 -0700

    lowercase all tags

--- classes/Tag.js
+++ classes/Tag.js
@@ -4,6 +4,7 @@ tag = {};
 
 
 tag.createTag = function (tag_name) {
+    tag_name = tag.toLowerCase();
     return new Promise(function (resolve, reject) {
         database.query(`INSERT INTO Tag(tag_name) VALUES ('${tag_name}') ON CONFLICT (tag_name) DO NOTHING`, (err, res) => {
             if (err) {
@@ -17,6 +18,7 @@ tag.createTag = function (tag_name) {
 
 
 tag.getArt = function (tag_name) {
+    tag_name = tag_name.toLowerCase();
     return new Promise(function(resolve, reject){
         database.query(`SELECT * FROM Art WHERE EXISTS (SELECT * FROM Has WHERE Has.tag_name = '${tag_name}' AND Has.art_id = Art.art_id)`, (err, res) => {
             if (err){ reject(err.detail) }
