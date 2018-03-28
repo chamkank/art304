e0d7758ed7c2dd0bf306e95d8d4228c3b5fd54dc
CK <chamkank@users.noreply.github.com>
2018-03-27 17:56:52 -0700

    fix the path bug

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(require('express-session')({ secret: config.get('passport.secret'), resave: false, saveUninitialized: false }));
